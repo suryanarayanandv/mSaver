5087d1559e99213dee682bb42df9b8b147a393c5
suryanarayanandv <dbvenkat857@gmail.com>
2023-07-24 15:24:02 +0530

    support: multi letters

--- server/saver/views.py
+++ server/saver/views.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
-from scrap import findMatches, fetchWords
+from scrap import generateMappingFile, findMatches
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -11,19 +11,24 @@ def configure(request):
     if (request.method == "POST"):
         # GET the letter from the request body
         body = request.read().decode("utf-8")
-        MAPPINGS = fetchWords(json.loads(body)['letter'])
+        letters = json.loads(body)['letters']
         
-        with open("mappings.json", "w") as f:
-            json.dump(MAPPINGS, f)
+        # Generating word-config/mappings.json
+        status = generateMappingFile(letters)
         
-        return HttpResponse(HTTPStatus.OK)
+        # Checking if the file was generated successfully
+        if not status:
+            return HttpResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+        
+        return HttpResponse(status=HTTPStatus.OK)
 
 @csrf_exempt
-def search(request):
-    with open("mappings.json", "r") as f:
-        MAPPINGS = json.load(f)
-    
+def search(request):    
     body = request.read().decode("utf-8")
-    payload = findMatches(json.loads(body)['word'], MAPPINGS)
-    print(payload)
+    word = json.loads(body)['word']
+    payload = findMatches(word)
+
+    if (payload == False):
+        return HttpResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+
     return HttpResponse(payload)
